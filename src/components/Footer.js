ea60547cd0c53965eeebc62b6b6eca2a17c9099d
Justin Moy <moy.justin94@gmail.com>
2022-10-13 17:41:15 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>
+            Justin Moy 2022
+        </p>
+        <Link to="/about">About</Link>
+    </footer>
+  )
+}
+
+export default Footer
